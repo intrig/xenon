4d36602e75d416cb08006f90d7672b74e9e5c490
Mark Beckwith <wythe@intrig.com>
2019-11-15 17:31:45 -0600

    Upgrade to r15 (#48)
    Support r15 of 36.331
    
    Updated README instructions
    Fixed up asnstrip.cpp to work with pdftotext output
    fixed up and added r15 36.331.xddl
    
    The new r15 xddl renamed some fields so updated icd_gold.xv
    remove asnstrip from macos build
    
    Signed-off-by: Mark Beckwith <wythe@intrig.com>

--- tools/asnstrip.cpp
+++ tools/asnstrip.cpp
@@ -1,126 +1,67 @@
 #include <algorithm>
+#include <experimental/filesystem>
 #include <fstream>
 #include <string>
 #include <vector>
-
 #include <xenon/ict/command.h>
 #include <xenon/xddl_code.h>
 
 using std::cerr;
 using std::cout;
 using std::endl;
+namespace fs = std::experimental::filesystem;
 
-class TextSource {
-  public:
-    TextSource(std::string filename) : filename(filename) {
-        std::ifstream is(filename.c_str());
-        std::string line;
+template <typename T>
+inline auto read_text_file(const std::string &filename, T op) {
+    if (!fs::exists(filename))
+        IT_PANIC(filename << " doesn't exist");
 
-        if (!is.good())
-            IT_PANIC("bad xml filename: \"" << filename << "\"");
+    if (!fs::is_regular_file(filename))
+        IT_PANIC(filename << " is not a regular file");
 
-        while (!is.eof()) {
-            std::getline(is, line);
-            ict::normalize(line);
-            lines.push_back(line);
+    std::ifstream file(filename.c_str());
+    if (!file.good())
+        IT_PANIC("cannot open " << filename);
+
+    std::vector<std::string> contents;
+    for (std::string line; std::getline(file, line);) {
+        if (line.back() == '\r')
+            line.pop_back();
+        op(line);
+    }
+}
+void convert(const std::string &txt_file) {
+    bool asn = false;
+    bool skip_next = false;
+
+    read_text_file(txt_file, [&](auto line) {
+        if (line.find("-- ASN1START") != std::string::npos)
+            asn = true;
+        else if (line.find("-- ASN1STOP") != std::string::npos)
+            asn = false;
+        else if (asn) {
+            if (line.find("      ETSI") != std::string::npos)
+                skip_next = true;
+            else if (skip_next)
+                skip_next = false;
+            else
+                std::cout << line << '\n';
         }
-    }
-
-    std::string add_line(std::string const &src) {
-        if ((src.find("ETSI") == std::string::npos) &&
-            (src.find("---------") == std::string::npos))
-            return src;
-        return "";
-    }
-
-    std::string asn() {
-        enum State {
-            WaitingForStart,
-            Extracting,
-        };
-        State state = WaitingForStart;
-        std::string asn;
-        for (auto it = lines.begin(); it != lines.end(); ++it) {
-            switch (state) {
-            case WaitingForStart:
-                if (it->find("DEFINITIONS") != std::string::npos) {
-                    asn += add_line(*it);
-                    asn += '\n';
-                    state = Extracting;
-                }
-                break;
-            case Extracting:
-                asn += add_line(*it);
-                asn += '\n';
-                if (it->find("END") != std::string::npos)
-                    state = WaitingForStart;
-                break;
-            }
-        }
-        return asn;
-    }
-
-    std::string asn_m() {
-        enum State {
-            WaitingForStart,
-            Extracting,
-        };
-
-        State state = WaitingForStart;
-        std::string asn;
-
-        for (auto it = lines.begin(); it != lines.end(); ++it) {
-            // skip page footer text
-            if ((it->find("ETSI") == std::string::npos) &&
-                (it->find("---------") == std::string::npos)) {
-                switch (state) {
-                case WaitingForStart:
-                    if (it->find("-- ASN1START") != std::string::npos) {
-                        // PRINT("start found");
-                        state = Extracting;
-                    }
-                    break;
-                case Extracting:
-                    if (it->find("-- ASN1STOP") != std::string::npos) {
-                        // PRINT("end found");
-                        state = WaitingForStart;
-                    } else {
-                        // PRINT("adding: " << *it);
-                        asn += *it;
-                        asn += '\n';
-                    }
-                    break;
-                }
-            }
-        }
-
-        return asn;
-    }
-
-    std::string filename;
-    std::vector<std::string> lines;
-};
+    });
+}
 
 int main(int argc, char **argv) {
-    bool markers = false;
-
     std::string xml_patch;
     try {
         ict::command line("asn-strip", "Strip ASN out of text file",
                           "asn-strip textfile");
-        line.add(ict::Option("markers", 'm',
-                             "file has ASN1START/ASN1STOP comment markers",
-                             [&] { markers = true; }));
         line.parse(argc, argv);
 
         if (line.targets.size() != 1) {
             line.help();
             IT_PANIC("exactly one text file must be specified");
         }
-
-        TextSource source(line.targets[0]);
-
-        cout << (markers ? source.asn_m() : source.asn()) << endl;
+        convert(line.targets[0]);
 
     } catch (std::exception &e) {
         cerr << e.what() << endl;
